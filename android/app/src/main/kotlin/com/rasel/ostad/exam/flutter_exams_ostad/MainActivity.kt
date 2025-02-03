92038bc4e7092d8c85c227685a4bc4458df945f9
Rasel Ahamed Adnan <raselahmedadnan@gmail.com>
2025-02-03 20:28:07 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/rasel/ostad/exam/flutter_exams_ostad/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.rasel.ostad.exam.flutter_exams_ostad
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+
+
+// এখানে কোন কিছু নেই দয়া করে Branches চেক করুন
+
+
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      //home:,
+    );
+  }
+}
